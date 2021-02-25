462106112dc74c5ccc14c48156a0fe3d7d264da2
Robert Espinoza <robertdev08@gmail.com>
2021-02-25 16:53:38 -0500

    Add auth middleware to endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express, {Request, Response} from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import { requireAuth } from './security/auth';
 
 (async () => {
 
@@ -31,7 +32,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   //! END @TODO1
 
-  app.get("/filteredimage", async (req: Request, res: Response) => {
+  app.get("/filteredimage", requireAuth, async (req: Request, res: Response) => {
     const imageUrl = req.query.image_url;
 
     if (!imageUrl) {
